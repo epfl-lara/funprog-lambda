81e1b471d1bdfb32e0dd576a964fac5aa21256eb
Viktor Kuncak <viktor.kuncak@epfl.ch>
2019-07-03 17:46:33 +0200

    printing like it's Haskell

--- core/src/main/scala/Main.scala
+++ core/src/main/scala/Main.scala
@@ -1,11 +1,8 @@
 object Main {
-
   def main(args: Array[String]): Unit = {
     import verified._
     println(s"Running test(3) ==> ${test(3)}")
-    println(s"Lambda identity ==> ${printS(ex.second)}")
+    println(s"Lambda first ==> ${print.print(ex.twice_square)}")
     println("Here is another println.")
-  }
-  
+  } 
 }
-
--- verified/src/main/scala/verified.scala
+++ verified/src/main/scala/verified.scala
@@ -13,13 +13,37 @@ object verified {
     val id = Lam("x", Var("x"))
     val first = Lam("x", Lam("y", Var("x")))
     val second = Lam("x", Lam("y", Var("y")))
-  }
-  
-  def printS(t: Term): String = t match {
-    case Var(s) => s
-    case App(f, arg) => printS(f) + "(" + printS(arg) + ")"
-    case Lam(arg, body) => "(" + arg + "=> " + printS(body) + ")"
+    val twice = Lam("f", Lam("x", App(Var("f"), App(Var("f"), Var("x")))))
+    val square = Lam("i", App(App(Var("*"), Var("i")), Var("i")))
+    val twice_square = App(App(twice, square), Var("3"))
   }  
+
+  object print {
+    def simple(t: Term): String = t match {
+      case Var(s) => s
+      case App(f, arg) => simple(f) + "(" + simple(arg) + ")"
+      case Lam(arg, body) => "(" + arg + "=> " + simple(body) + ")"
+    }
+    def print(t: Term): String = t match {
+      case Var(s) => s
+      case l@Lam(_,_) => "\\" + args(l)
+      case App(_,_) => apps(t)
+    }
+    def parenPrint(t: Term): String = t match {
+      case Var(s) => s
+      case _ => "(" + print(t) + ")"
+    }
+    def args(t: Lam): String = t match {
+      case Lam(arg, body) => arg + (body match {
+	  case l@Lam(_, _) => " " + args(l)
+	  case _ => "->" + print(body)
+      })
+    }
+    def apps(t: Term): String = t match {
+      case App(f, e) => apps(f) + " " + parenPrint(e)
+      case _ => parenPrint(t)
+    }
+  }
 
   def test(x: Int): Boolean = {
     require (0 < x && x < 100)
