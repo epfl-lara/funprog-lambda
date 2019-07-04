89c98268e30eaa244c9cad32fa579ea262c71906
Viktor Kuncak <viktor.kuncak@epfl.ch>
2019-07-04 23:30:29 +0200

    capture avoiding subst, if expressions, prim ops

--- verified/src/main/scala/LambdaCalculus.scala
+++ verified/src/main/scala/LambdaCalculus.scala
@@ -22,22 +22,84 @@ object LambdaCalculus {
 	if (s==v) t else Lam(s, replace(v,replW,body))
     }
 
-  def betaReduce(t: App): Option[Term] = t match {
-    case App(Lam(s,body),arg) => Some(replace(s,arg,body))
+  @extern
+  def strOf(k: Long): String =
+    if (k <= 0) "" else strOf(k-1) + "'"
+
+  def getFresh(s: String, k: Long, avoid: List[String]): String = {
+    val s1 = s + strOf(k)
+    if (avoid.contains(s1)) getFresh(s, k+1, avoid)
+    else s1
+  }
+  
+  def replaceSafe(v: String, replW: Term, t: Term): Term =
+    t match {
+      case Var(s) => if (s==v) replW else t
+      case App(f, arg) => App(replaceSafe(v,replW,f),
+			      replaceSafe(v,replW,arg))
+      case Lam(s, body) => 
+	if (s==v) t else {
+	  val fv = free_vars(replW)
+	  if (fv.contains(s)) {
+	    val s1 = getFresh(s, 1, fv)
+	    val body1 = replace(s, Var(s1), body)
+	    Lam(s1, replace(v, replW, body1))
+	  } else Lam(s, replace(v, replW, body))
+	}
+    }
+
+  val plusSym = "+"
+  val minusSym = "-"
+  val ltSym = "<"
+  val trueSym = "1"
+  val falseSym = "0"
+  val ifSym = "if"
+  
+  def binOp(op: String, x: Long, y: Long): Option[Long] = {
+    if (op==plusSym) Some(x + y)
+    else if (op==minusSym) Some(x - y)
+    else if (op==ltSym) Some(if (x < y) 1 else 0)
+    else None()
+  }
+  
+  @extern
+  def binOpStr(op: String, x: String, y: String): Option[Term] = {
+    try {
+      binOp(op, x.toLong, y.toLong) match {
+	case Some(i) => Some(Var(i.toString))
+	case None() => None()
+      }
+    } catch {
+      case _: java.lang.NumberFormatException => None()
+    }
+  }
+
+  def betaReduce(t: Term): Option[Term] = t match {
+    case App(Lam(s,body),arg) =>
+      Some(replaceSafe(s,arg,body))
+    case App(App(App(Var(ifSym),
+          Var(cond)), trueBranch), falseBranch) =>
+      if (cond==trueSym) Some(trueBranch)
+      else if (cond==falseSym) Some(falseBranch)
+      else None()
+    case App(App(Var(s), Var(n1)), Var(n2)) => binOpStr(s,n1,n2)    
     case _ => None()
   }
   
-  def cbvReduce1(t: Term): Option[Term] = t match {
-    case App(Lam(s,body),arg) => Some(replace(s,arg,body))
-    case App(f, arg) => cbvReduce1(f) match {
-      case Some(fRed) => Some(App(fRed, arg))
-      case None() => cbvReduce1(arg) match {
-	case Some(argRed) => Some(App(f, argRed))
-	case None() => None()
+  def cbvReduce1(t: Term): Option[Term] =
+    betaReduce(t) match {
+      case Some(tr) => Some(tr)
+      case None() => t match {
+	case App(f, arg) => cbvReduce1(f) match {
+	  case Some(fRed) => Some(App(fRed, arg))
+	  case None() => cbvReduce1(arg) match {
+	    case Some(argRed) => Some(App(f, argRed))
+	    case None() => None()
+	  }
+	}
+	case _ => None()
       }
     }
-    case _ => None()
-  }
 
   def cbvTrace(t: Term, max: Int): List[Term] = {
     require(0 <= max)
@@ -48,4 +110,3 @@ object LambdaCalculus {
     }
   }
 }
-
--- verified/src/main/scala/examples.scala
+++ verified/src/main/scala/examples.scala
@@ -8,26 +8,36 @@ object ex {
   val id = Lam("y", Var("y"))
   val first = Lam("x", Lam("y", Var("x")))
   val second = Lam("x", Lam("y", Var("y")))
-  val self = Lam("x", App(Var("x"), Var("x")))
+  val self = Lam("x", App(Var("x"), Var("x")))  
   val selfid = App(App(self, id), Var("a"))
+  val omega = App(self,self) 
   val twice = Lam("f", Lam("x", App(Var("f"), App(Var("f"), Var("x")))))
-  val square = Lam("i", App(App(Var("times"), Var("i")), Var("i")))
-  val twice_square = App(App(twice, square), Var("three"))
+  val doubling = Lam("i", App(App(Var(plusSym), Var("i")), Var("i")))
+  val twice_doubling = App(App(twice, doubling), Var("3"))
+  val abs = Lam("x",
+		App(App(App(Var(ifSym),
+                  App(App(Var("<"), Var("x")), Var("0"))),
+		  App(App(Var(minusSym), Var("0")), Var("x"))),
+		  Var("x")))
+  val abs42 = App(abs, Var("42"))
+  val firstAB = App(App(first, Var("a")), Var("b"))
+  val captureFirst = App(App(first, Var("y")), Var("z"))
 
   val terms = List(
     "id" -> id,
     "self id" -> selfid,
-    "twice_square" -> twice_square    
+    "twice_doubling" -> twice_doubling,
+    "abs42" -> abs42,
+    "firstAB" -> firstAB,
+    "captureFirst" -> captureFirst
   )
 
-  def showNamedTerm(p: (String,Term)): String =
-    p._1 + ":  " + print.print(p._2)
   def showTrace(t: Term): String =
-    List.mkString(cbvTrace(t, 20), "\n==>", print.print)
+    List.mkString(cbvTrace(t, 50), "\n ==> ", print.print)
+  
+  def showNamedTerm(p: (String,Term)): String =
+    p._1 + ":  " + showTrace(p._2)
 
   val toShow: String =
-    List.mkString(terms, "\n", showNamedTerm) + "\n" +
-    showTrace(twice_square) + "\n" +
-    showTrace(selfid)
-  
+    List.mkString(terms, "\n", showNamedTerm) 
 }
