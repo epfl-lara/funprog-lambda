1d59840a031913784c36e1cb7996d3460c6968fa
Viktor Kuncak <viktor.kuncak@epfl.ch>
2019-11-27 12:37:38 +0100

    expression language with BigInt

--- build.sbt
+++ build.sbt
@@ -2,6 +2,9 @@ ThisBuild / version      := "0.1.0-SNAPSHOT"
 ThisBuild / organization := "com.example"
 ThisBuild / scalaVersion := "2.12.8"
 
+resolvers += Resolver.bintrayRepo("epfl-lara", "maven")
+libraryDependencies += "ch.epfl.lara" %% "scallion" % "0.4"
+
 lazy val core = project
   .in(file("core"))
   .settings(
--- recursive/src/main/scala/ExpressionLanguage.scala
+++ recursive/src/main/scala/ExpressionLanguage.scala
@@ -5,7 +5,8 @@ import stainless.annotation._
 object ExpressionLanguage {
 
   sealed abstract class Expr
-  case class IntConst(c: Int) extends Expr
+  case class Const(c: BigInt) extends Expr
+  case class Val(name: String) extends Expr
   case class BinOp(op: BinaryOperator, arg1: Expr, arg2: Expr) extends Expr
   case class IfNonzero(cond: Expr, trueE: Expr, falseE: Expr) extends Expr
 
@@ -13,21 +14,39 @@ object ExpressionLanguage {
   case object Plus extends BinaryOperator
   case object Minus extends BinaryOperator
   case object Times extends BinaryOperator
+  case object Power extends BinaryOperator
   case object LessEq extends BinaryOperator
 
-  def evalBinOp(op: BinaryOperator)(x: Int, y: Int): Int = op match {
+  def evalBinOp(op: BinaryOperator)(x: BigInt, y: BigInt): BigInt = op match {
     case Plus => x + y
     case Minus => x - y
     case Times => x * y
+    case Power => x.pow(y.toInt)
     case LessEq => if (x <= y) 1 else 0
   }
 
-  def evalExpr(e: Expr): Int = e match {
-    case IntConst(c) => c
+  def evalExpr(e: Expr): BigInt = e match {
+    case Const(c) => c
+    case Val(_) => 0
     case BinOp(op, arg1, arg2) =>
       evalBinOp(op)(evalExpr(arg1), evalExpr(arg2))
     case IfNonzero(cond, trueE, falseE) =>
       if (evalExpr(cond) != 0) evalExpr(trueE)
       else evalExpr(falseE)
   }
+
+  def expr1 = BinOp(Times, Const(6), Const(7))
+  def cond1 = BinOp(LessEq, expr1, Const(50))
+  def expr2 = IfNonzero(cond1, Const(10), Const(20))
+  def expr3 = BinOp(Power, Const(10), Const(100))
+
+  def main(args: Array[String]): Unit = {
+    print("expr1 --> ")
+    println(evalExpr(expr1))
+    print("expr2 --> ")
+    println(evalExpr(expr2))
+    print("expr3 --> ")
+    println(evalExpr(expr3))
+  }
+
 }
